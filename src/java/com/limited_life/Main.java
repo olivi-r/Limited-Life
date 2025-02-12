ed8e6a7db4a412456cecfc0a912bf943b12db0db
Olivia Ryan <olivia.r.dev@gmail.com>
2025-02-12 02:07:14 +0000

    Implement /time command

--- src/java/com/limited_life/Main.java
+++ src/java/com/limited_life/Main.java
@@ -1,11 +1,38 @@
 package com.limited_life;
 
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
+import com.limited_life.command.TimeCommand;
+
+public class Main extends JavaPlugin implements Listener {
+	NamespacedKey timeKey;
 
 	@Override
 	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
+		timeKey = new NamespacedKey(this, "time");
+
+		TimeCommand timeCommand = new TimeCommand(this);
+		getCommand("time").setExecutor(timeCommand);
 	}
 
+	@EventHandler
+	public void onPlayerJoin(PlayerJoinEvent event) {
+//		set time to 24hrs for new players
+		Player player = event.getPlayer();
+		if (!player.hasPlayedBefore()) {
+			player.getPersistentDataContainer().set(timeKey, PersistentDataType.INTEGER, 24 * 60 * 60);
+		}
+	}
+
+	public Integer getTime(Player player) {
+		return player.getPersistentDataContainer().get(timeKey, PersistentDataType.INTEGER);
+	}
 }
---
+++ src/java/com/limited_life/command/TimeCommand.java
@@ -0,0 +1,41 @@
+package com.limited_life.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+
+import com.limited_life.Main;
+
+public class TimeCommand implements CommandExecutor, TabCompleter {
+	Main plugin;
+
+	public TimeCommand(Main plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (sender instanceof Player) {
+			Integer time = plugin.getTime((Player) sender);
+			time = time == null ? 0 : time;
+			int seconds = time % 60;
+			time /= 60;
+			int minutes = time % 60;
+			time /= 60;
+			sender.sendMessage(String.format("You have %02d:%02d:%02d left", time, minutes, seconds));
+			return true;
+		}
+		return false;
+	}
+
+}
