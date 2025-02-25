964d8eef5507954f3a0501113a7e72793d226f59
Olivia Ryan <olivia.r.dev@gmail.com>
2025-02-25 00:42:09 +0000

    Make offline players time decrease

--- src/java/com/limited_life/Main.java
+++ src/java/com/limited_life/Main.java
@@ -1,8 +1,14 @@
 package com.limited_life;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +25,11 @@ import com.limited_life.command.GiveMinutesCommand;
 import com.limited_life.command.SetTimeCommand;
 import com.limited_life.command.TimeCommand;
 import com.limited_life.command.UnfreezeCommand;
+
+import net.querz.nbt.io.NBTUtil;
+import net.querz.nbt.io.NamedTag;
+import net.querz.nbt.tag.CompoundTag;
+import net.querz.nbt.tag.IntTag;
 
 public class Main extends JavaPlugin implements Listener {
 	NamespacedKey timeKey;
@@ -101,10 +112,10 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		Player player = event.getEntity();
-		if (getLastDeath(player) == 0) {
+		if (getKillProtection(player) == 0) {
 //			take hour of total time and set spawn kill protection to 5mins
 			setTime(player, getTime(player) - 3600);
-			setLastDeath(player, killProtectionTime);
+			setKillProtection(player, killProtectionTime);
 			refresh(player);
 		}
 	}
@@ -115,7 +126,7 @@ public class Main extends JavaPlugin implements Listener {
 		Player player = event.getPlayer();
 		if (!player.hasPlayedBefore()) {
 			setTime(player, 86400);
-			setLastDeath(player, killProtectionTime);
+			setKillProtection(player, killProtectionTime);
 		}
 		refresh(player);
 	}
@@ -128,26 +139,52 @@ public class Main extends JavaPlugin implements Listener {
 		player.getPersistentDataContainer().set(timeKey, PersistentDataType.INTEGER, Integer.max(0, value));
 	}
 
-	public int getLastDeath(Player player) {
+	public int getKillProtection(Player player) {
 		return Integer.max(0, player.getPersistentDataContainer().get(killProtectionKey, PersistentDataType.INTEGER));
 	}
 
-	public void setLastDeath(Player player, int value) {
+	public void setKillProtection(Player player, int value) {
 		player.getPersistentDataContainer().set(killProtectionKey, PersistentDataType.INTEGER, Integer.max(0, value));
 	}
 
 	public void unfreeze() {
+		Path playerdata = Bukkit.getWorlds().getFirst().getWorldFolder().toPath().resolve("playerdata");
 		freeze();
 		timerThread = new Thread(new Runnable() {
 			public void run() {
 				try {
 					while (true) {
-						Thread.sleep(1000);
-						Bukkit.getOnlinePlayers().forEach(player -> {
-							setTime(player, getTime(player) - 1);
-							setLastDeath(player, getLastDeath(player) - 1);
-							refresh(player);
-						});
+						try {
+							Thread.sleep(1000);
+							OfflinePlayer[] players = Bukkit.getOfflinePlayers();
+							for (int i = 0; i < players.length; i++) {
+								OfflinePlayer offlinePlayer = players[i];
+								Player onlinePlayer = offlinePlayer.getPlayer();
+								if (onlinePlayer != null) {
+									setTime(onlinePlayer, getTime(onlinePlayer) - 1);
+									setKillProtection(onlinePlayer, getKillProtection(onlinePlayer) - 1);
+									refresh(onlinePlayer);
+								} else {
+									// OfflinePlayer doesnt load PersistantDataContainer
+									// need write directly to <player uuid>.dat nbt file
+									UUID id = offlinePlayer.getUniqueId();
+									File playerDataFile = playerdata.resolve(String.join("", id.toString(), ".dat"))
+											.toFile();
+									NamedTag nbtData = NBTUtil.read(playerDataFile);
+									CompoundTag bukkitValues = ((CompoundTag) nbtData.getTag())
+											.getCompoundTag("BukkitValues");
+									IntTag timeTag = bukkitValues.getIntTag(timeKey.toString());
+									IntTag killProtectionTag = bukkitValues.getIntTag(killProtectionKey.toString());
+
+									// Offline players lose time twice as fast
+									timeTag.setValue(Integer.max(0, timeTag.asInt() - 2));
+									killProtectionTag.setValue(Integer.max(0, killProtectionTag.asInt() - 1));
+									NBTUtil.write(nbtData, playerDataFile);
+								}
+							}
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
 					}
 				} catch (InterruptedException err) {
 					Thread.currentThread().interrupt();
